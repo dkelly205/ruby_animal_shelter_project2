4e3d9c41a014c60008e8d43481ae79d4fc7f8b3a
Daniel Kelly <Danny@daniels-air.home>
2018-05-19 02:03:12 +0100

    adds update, self.all self.find methods to animal class

--- models/animal.rb
+++ models/animal.rb
@@ -21,15 +21,53 @@ class Animal
   end
 
   def save()
-  sql = "INSERT INTO animals (name, type, breed, admission_date, adoptable, image, owner_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *"
-  values = [@name, @type, @breed, @admission_date, @adoptable, @image, @owner_id]
-  @id = SqlRunner.run(sql, values)[0]["id"].to_i()
-end
+    sql = "INSERT INTO animals (name, type, breed, admission_date, adoptable, image, owner_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *"
+    values = [@name, @type, @breed, @admission_date, @adoptable, @image, @owner_id]
+    @id = SqlRunner.run(sql, values)[0]["id"].to_i()
+  end
 
-def self.delete_all
-  sql = "DELETE FROM animals"
-  SqlRunner.run(sql)
-end
+  def self.delete_all
+    sql = "DELETE FROM animals"
+    SqlRunner.run(sql)
+  end
+
+  def self.delete(id)
+     sql = "DELETE FROM albums
+     WHERE id = $1"
+     values = [id]
+     SqlRunner.run( sql, values )
+   end
+
+  def self.all
+    sql = "SELECT * FROM animals"
+    animals = SqlRunner.run(sql)
+    result = animals.map{|animal| Animal.new(animal)}
+    return result
+  end
+
+  def self.find(id)
+        sql = "SELECT * FROM animals WHERE id = $1"
+        values = [id]
+        animal = SqlRunner.run(sql, values)
+        result = Animal.new(animal.first)
+        return result
+      end
+
+
+
+  def update()
+    sql = "UPDATE animals
+    SET
+    (
+      name, type, breed, admission_date, adoptable, image, owner_id
+    ) =
+    (
+      $1, $2, $3, $4, $5, $6, $7
+    )
+    WHERE id = $8"
+    values = [@name, @type, @breed, @admission_date, @adoptable, @image, @owner_id]
+    SqlRunner.run( sql, values )
+  end
 
 
 
