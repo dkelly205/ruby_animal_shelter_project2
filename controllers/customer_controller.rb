1e778ef036564bde899e2d0020bdfe5ad50f972d
Daniel Kelly <Danny@daniels-air.home>
2018-05-21 21:12:10 +0100

    adds customers/new.erb, adds route to customers controller, can now create a customer

--- controllers/customer_controller.rb
+++ controllers/customer_controller.rb
@@ -7,3 +7,14 @@ get '/customers' do
   @customers = Customer.all()
   erb(:"customers/index")
 end
+
+# create
+post '/customers' do
+  @customer = Customer.new(params)
+  @customer.save
+  redirect to "/customers"
+end
+
+get "/customers/new" do
+  erb(:"customers/new")
+end
