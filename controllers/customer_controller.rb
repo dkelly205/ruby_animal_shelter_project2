8aa7bd02f939e752c5fdc955a53866736278a57e
Daniel Kelly <Danny@daniels-air.home>
2018-05-22 11:56:52 +0100

    adds customers/edit.erb, can now update customers details

--- controllers/customer_controller.rb
+++ controllers/customer_controller.rb
@@ -35,3 +35,14 @@ post "/customers/:id/delete" do
   Customer.delete(params[:id])
   redirect to "/customers"
 end
+
+get "/customers/:id/edit" do
+  @customer = Customer.find(params[:id])
+  erb(:"customers/edit")
+end
+
+post "/customers/:id" do
+  @customer = Customer.new(params)
+  @customer.update()
+  redirect to "/customers"
+end
